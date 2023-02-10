d57ed726ddf9790b84707cbec3cd0f4d67d7d630
gregnuj <greg@terra.money>
2023-02-09 20:03:36 -0600

    update script to build locally

--- scripts/join-testnet.sh
+++ scripts/join-testnet.sh
@@ -1,33 +1,74 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 # This script will join the testnet and start the node
-
-declare CHAIN_ID
-declare PREFIX
-declare DENOM
-declare BINARY
-declare GOA_VERSION="v0.0.1-goa"
-declare readonly BIN_PATH="${HOME}/bin"
+declare GTMPDIR="${TMPDIR:-/tmp}"
+declare readonly OS=$(uname -s)
+declare readonly PLATFORM=$(uname -m)
+declare readonly GO_VERSION="1.19.5"
+declare readonly GO_MD5="09e7f3b3ef34eb6099fe7312ecc314be"
+declare readonly GOA_VERSION="v0.0.1-goa"
+declare readonly BIN_PATH="${HOME}/go/bin/"
 declare readonly GITHUB_REPO="terra-money/alliance"
 declare readonly GITHUB_URL="https://github.com/${GITHUB_REPO}"
 declare readonly GITHUB_RAW="https://raw.githubusercontent.com/${GITHUB_REPO}/${GOA_VERSION}"
 
-download_binaries (){
-    mkdir -p "${HOME}/bin"
-    GOA_GZ="testnets-$(uname -s)-$(uname -m).tar.gz"
-    GOA_DOWNLOAD="${GITHUB_URL}/releases/download/${GOA_VERSION}/${GOA_GZ}"
-    echo "Downloading ${GOA_DOWNLOAD}"
-    curl -sSL "${GOA_DOWNLOAD}" | tar -xz -C "${HOME}/bin"
+
+error(){
+    echo "Error: $1"
+    exit 1
 }
 
-verify_binary(){
-    local binary=$1
-    if [ ! -f "$binary" ]; then
-        echo "Binary $binary does not exist"
-        exit 1
+download_go (){
+    local tmpdir=$1
+    if [ $OS == "Linux" ] && [ $PLATFORM == "x86_64" ]; then
+       GO_GZ="go${GO_VERSION}.linux-amd64.tar.gz" 
+    elif [ $OS == "Darwin" ] && [ $PLATFORM == "arm64" ]; then
+       GO_GZ="go${GO_VERSION}.darwin-arm64.tar.gz"
+    else
+        error "Unsupported OS/Platform"
     fi
+    GO_DOWNLOAD="https://go.dev/dl/${GO_GZ}"
+    cd ${GTMPDIR}
+    if [ ! -f "${GO_GZ}" ]; then
+        echo "Downloading go from ${GO_DOWNLOAD}"
+        curl -L "${GO_DOWNLOAD}"  -o ${GO_GZ}
+    fi
+    echo "Extracting ${GO_GZ}"
+    # need to check md5sum
+    tar -xz ${GO_GZ} -C "${tmpdir}"
+    echo
+}
+
+download_source (){
+    local tmpdir=$1
+    GOA_GZ="${GOA_VERSION}.tar.gz" 
+    GOA_DOWNLOAD="${GITHUB_URL}/archive/refs/tags/${GOA_GZ}"
+    cd ${GTMPDIR}
+    if [ ! -f "${GOA_GZ}" ]; then
+        echo "Downloading alliance from ${GOA_DOWNLOAD}"
+        curl -sSL "${GOA_DOWNLOAD}" 
+    fi
+    # need to check md5sum
+    echo "Extracting ${GOA_GZ}"
+    tar -xz ${GOA_GZ} -C "${tmpdir}"
+    echo
+}
+
+create_binary(){
+    local prefix=$1
+    local tmpdir=$(mktemp -d)
+    download_go ${tmpdir}
+    download_source ${tmpdir}
+    cd ${tmpdir}/alliance*
+    export PATH="${tmpdir}/go/bin:${PATH}"
+    export GOROOT="${tmpdir}/go"
+    make build-alliance ACC_PREFIX="${prefix}"
+    mkdir -p "${BIN_PATH}"
+    cp "build/${prefix}d" "${BIN_PATH}"
+    echo "Binary is located at ${BIN_PATH}${prefix}d"
+    rm -rf ${tmpdir}
 }
 
 verify_chain_id (){
@@ -52,15 +93,11 @@ verify_chain_id (){
     esac
 }
 
-get_binary_name(){
+get_binary(){
     local chain_id=$1
     echo "$(cut -d "-" -f1 <<< $chain_id)d"
 }
 
-get_binary_path(){
-    local chain_id=$1
-    echo "${BIN_PATH}/$(get_binary_name $chain_id)"
-}
 
 get_prefix(){
     local chain_id=$1
@@ -98,18 +135,28 @@ get_peers(){
     done | paste -sd "," -
 }
 
+
 parse_options(){
     while [ $# -gt 0 ]; do
         case "$1" in
+            -b|--binary)
+                BINARY=$2
+                shift 2
+                ;;
             -c|--chain-id)
                 CHAIN_ID=$(verify_chain_id $2)
-                PREFIX=$(get_prefix $CHAIN_ID)
-                DENOM=$(get_denom $CHAIN_ID)
-                BINARY=$(get_binary_path $CHAIN_ID)
+                shift 2
+                ;;
+            -d|--denom)
+                DENOM=$2
                 shift 2
                 ;;
             -m|--moniker)
                 MONIKER=$2
+                shift 2
+                ;;
+            -p|--prefix)
+                PREFIX=$2
                 shift 2
                 ;;
             --)
@@ -122,21 +169,35 @@ parse_options(){
                 ;;
         esac
     done
+    set +u
+    if [ -z "${BINARY}" ]; then
+        BINARY=$(get_binary $CHAIN_ID)
+    fi
+    if [ -z "${PREFIX}" ]; then
+        PREFIX=$(get_prefix $CHAIN_ID)
+    fi
+    if [ -z "${DENOM}" ]; then
+        DENOM=$(get_denom $CHAIN_ID)
+    fi
+    set -u
 }
 
 main(){
     parse_options $@
-    rm -rf /Users/greg/.ordos
-
-    echo "Downloading binaries... This may take some time"
-    download_binaries
-
-    echo "Verify binary exists"
-    verify_binary "${BINARY}"
+    
+    # prepend local path
+    PATH="${BIN_PATH}:${PATH}"
+    
+    # check if binary exists
+    if [ -z "$(which ${BINARY})" ]; then 
+        create_binary "${PREFIX}"
+    fi
     
     echo "Initializing node"
     
-    $BINARY init "${MONIKER}" --chain-id "${CHAIN_ID}" 2>&1 | sed -e 's/{.*}//' 
+    if [ ! -f "${HOME}/.${PREFIX}/config/genesis.json" ]; then
+        $BINARY init "${MONIKER}" --chain-id "${CHAIN_ID}" 2>&1 | sed -e 's/{.*}//' 
+    fi
 
     echo "Downloading genesis file"
     curl -sSL "${GITHUB_RAW}/genesis/${CHAIN_ID}/genesis.json" -o "${HOME}/.${PREFIX}/config/genesis.json"
