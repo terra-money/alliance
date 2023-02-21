f17114d2d1445b0dc1108036143c386cb45dac4e
phamminh0811 <1phamminh0811@gmail.com>
2023-02-21 14:49:12 +0700

    Custom tally function on gov keeper

--- custom/gov/module.go
+++ custom/gov/module.go
@@ -1,8 +1,12 @@
 package gov
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
 	govmodule "github.com/cosmos/cosmos-sdk/x/gov"
+	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	customgovkeeper "github.com/terra-money/alliance/custom/gov/keeper"
@@ -19,5 +23,19 @@ func NewAppModule(cdc codec.Codec, keeper customgovkeeper.Keeper, accountKeeper 
 	return AppModule{
 		AppModule: govmodule,
 		keeper:    keeper,
+	}
+}
+
+// RegisterServices registers module services.
+// NOTE: Overriding this method as not doing so will cause a panic
+// when trying to force this custom keeper into a govkeeper
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	m := govkeeper.NewMigrator(am.keeper.Keeper)
+	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
+		panic(fmt.Sprintf("failed to migrate x/gov from version 1 to 2: %v", err))
+	}
+
+	if err := cfg.RegisterMigration(types.ModuleName, 2, m.Migrate2to3); err != nil {
+		panic(fmt.Sprintf("failed to migrate x/gov from version 2 to 3: %v", err))
 	}
 }
